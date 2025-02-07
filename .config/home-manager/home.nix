6a3e5d9e45648d3ce3fdecdd71230c269db7d4ae
Leonardo Cerasi <leo.cerasi@pm.me>
2025-02-07 21:56:16 +0100

    initial commit

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "leonardo";
+  home.homeDirectory = "/home/leonardo/";
+
+  home. stateVersion = "24.11";
+  programs.home-manager.enable = true;
+
+  # programs
+
+  home.packages = with pkgs; [
+    thunderbird
+    texlive.combined.scheme-full
+    zathura
+    xdotool # for zathura in neovim
+    pstree # for zathura in neovim
+
+    # LSP
+    lua-language-server
+    vim-language-server
+    pyright
+    rust-analyzer
+    ltex-ls
+    texlab
+    lua51Packages.lua-lsp
+    ccls
+  ];
+
+  programs.neovim = {
+    enable = true;
+
+    plugins = [
+      pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+      pkgs.vimPlugins.catppuccin-nvim
+      pkgs.vimPlugins.lualine-nvim
+      pkgs.vimPlugins.mini-nvim
+      pkgs.vimPlugins.nvim-tree-lua
+      pkgs.vimPlugins.todo-comments-nvim
+      pkgs.vimPlugins.vim-sleuth
+      pkgs.vimPlugins.vimtex
+      pkgs.vimPlugins.which-key-nvim
+      pkgs.vimPlugins.nvim-web-devicons
+      pkgs.vimPlugins.gitsigns-nvim
+
+      pkgs.vimPlugins.telescope-nvim
+      pkgs.vimPlugins.plenary-nvim
+      pkgs.vimPlugins.telescope-fzf-native-nvim
+      pkgs.vimPlugins.telescope-ui-select-nvim
+
+      pkgs.vimPlugins.nvim-lspconfig
+      pkgs.vimPlugins.nvim-cmp
+      pkgs.vimPlugins.cmp-nvim-lsp
+      pkgs.vimPlugins.luasnip
+      pkgs.vimPlugins.friendly-snippets
+      pkgs.vimPlugins.cmp_luasnip
+    ];
+
+    extraConfig = ":luafile ~/.config/nvim/lua/init.lua";
+  };
+}
---
+++ .nix-bkp/configuration.nix
@@ -0,0 +1,287 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader = {
+    systemd-boot.enable = false;
+
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    grub = {
+      enable = true;
+      devices = [ "nodev" ];
+      useOSProber = true;
+      efiSupport = true;
+
+#      extraEntries = ''
+#        menuentry "Windows" {
+#	  insmod part_gpt
+#	  insmod fat
+#	  insmod searc_fs_uuid
+#	  insmod chain
+#	  search --fs-uuid --set=root F156-7327
+#	  chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+#	}
+#     '';
+    };
+   };
+
+  networking.hostName = "desktop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # NVIDIA
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+
+    open = true;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      nvidiaBusId = "PCI:11:0:0";
+    };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "it";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "it2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.leonardo = {
+    isNormalUser = true;
+    description = "Leonardo Cerasi";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      thunderbird
+    ];
+  };
+
+  # timers
+  systemd.timers."nix-bkp" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "hourly";
+      Persistent = true;
+      Unit = "nix-bkp.service";
+    };
+  };
+
+  systemd.services."nix-bkp" = {
+    script = ''
+      cp /etc/nixos/configuration.nix /home/leonardo/dotfiles/.nix-bkp/
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    git-credential-manager
+    stow
+    gcc
+    clang
+    rustup
+    gnupg
+    pass
+    lshw
+    telegram-desktop
+    signal-desktop
+    pinentry
+    pinentry-curses
+    nautilus
+    os-prober
+    spotify
+    gnomeExtensions.appindicator
+    proton-pass
+    gnome-keyring
+    gnumake
+    cmake
+    unzip
+    ripgrep
+    home-manager
+    python3
+    bc
+    lua5_1
+    luarocks
+    pinentry-gnome3
+    kitty
+    nodejs
+    fd
+    xclip
+    jdk
+  ];
+
+  environment.variables = {
+    EDITOR = "nvim";
+    _JAVA_OPTIONS= "-Dawt.useSystemAAFontSettings=lcd";
+  };
+
+  # neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+
+  };
+   
+  # zsh
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # gpg
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # gnome
+  environment.gnome.excludePackages = with pkgs; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    eog         # image viewer
+    epiphany    # web browser
+    #gedit       # text editor
+    simple-scan # document scanner
+    totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    file-roller # archive manager
+    geary       # email client
+    seahorse    # password manager
+    snapshot    # camera
+
+    #gnome-calculator
+    gnome-calendar gnome-characters gnome-clocks gnome-contacts
+    gnome-font-viewer gnome-logs gnome-maps gnome-music gnome-screenshot
+    gnome-system-monitor gnome-weather gnome-disk-utility gnome-connections
+    gnome-tour
+    gnome-console
+  ];
+
+  services.xserver.excludePackages = [ pkgs.xterm ]; 
+
+  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
