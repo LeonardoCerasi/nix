90d3be42679da1202926effadeb95832ff89673e
Leonardo Cerasi <leo.cerasi@pm.me>
2025-03-18 18:40:20 +0100

    implemented home-manager in the flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
+      system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
 
     nixosConfigurations = {
 
       desktop = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+
+    };
+
+    homeConfigurations = {
+
+      leonardo = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
 
     };
