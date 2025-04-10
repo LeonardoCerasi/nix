167d634221d2b7a5c91d3fc8d10cfb6437cec998
LeonardoCerasi <leo.cerasi@pm.me>
2025-04-10 14:51:02 +0200

    minor change

--- users/leo/app/browser/librewolf/librewolf.nix
+++ users/leo/app/browser/librewolf/librewolf.nix
@@ -5,5 +5,5 @@
     librewolf
   ];
 
-  # home.file.".librewolf/773hjn3d.default/chrome/userChrome.css".source = ./userChrome.css;
+  # home.file.".librewolf/pu4fepw1.default/chrome/userChrome.css".source = ./userChrome.css;
 }
