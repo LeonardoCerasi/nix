e6595e57da5395d31cf5047868e2e68d01cf925a
LeonardoCerasi <leo.cerasi@pm.me>
2025-04-27 13:43:31 +0200

    changed vimtex compiler

--- modules/nvim/lua/plugins/vimtex.lua
+++ modules/nvim/lua/plugins/vimtex.lua
@@ -1,3 +1,16 @@
-vim.g.vimtex_compiler_engine = "lualatex"
+vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_compiler_latexmk = {
+  executable = 'latexmk',
+  options = {
+    '-verbose',
+    '-file-line-error',
+    '-synctex=1',
+    '-interaction=nonstopmode',
+    '-shell-escape',
+    '-pdf',
+    '-pdflatex=lualatex',
+  },
+}
+
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_complete_enabled = true
