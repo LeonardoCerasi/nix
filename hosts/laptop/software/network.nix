5c97dea672e4bd0b1df26cfc8190c7e006ea2975
LeonardoCerasi <leo.cerasi@pm.me>
2025-05-05 00:27:36 +0200

    local network

--- hosts/laptop/software/network.nix
+++ hosts/laptop/software/network.nix
@@ -4,7 +4,16 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager.enable = true;
+
+    interfaces.enp3s0 = {
+      ipv4.addresses = [{
+        address = "10.0.0.2";
+        prefixLength = 24;
+      }];
+    };
+  };
 
   services.openssh = {
     enable = true;
